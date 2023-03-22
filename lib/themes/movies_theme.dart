084aa592df14104c32a14b35b607a89dd14ba50d
Cecilia Rivero <contact@ceciliarivero.com>
2023-03-22 20:18:04 +0000

    Update MoviesTheme.

--- lib/themes/movies_theme.dart
+++ lib/themes/movies_theme.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class MoviesTheme {
   ThemeData dark = ThemeData(
     brightness: Brightness.dark,
-    primaryColor: Colors.black38,
+    primaryColor: Colors.black,
+    scaffoldBackgroundColor: Colors.black,
     fontFamily: 'NotoSans',
     textTheme: MoviesTextTheme().regular,
   );
@@ -13,27 +14,34 @@ class MoviesTextTheme {
     displayLarge: TextStyle(
       fontSize: 72.0,
       fontWeight: FontWeight.bold,
+      color: Colors.white70,
     ),
     titleLarge: TextStyle(
       fontSize: 22.0,
+      color: Colors.white70,
     ),
     titleMedium: TextStyle(
       fontSize: 20.0,
+      color: Colors.white70,
     ),
     titleSmall: TextStyle(
       fontSize: 16.0,
+      color: Colors.white70,
     ),
     bodyLarge: TextStyle(
       fontSize: 14.0,
-      height: 1.5,
+      height: 2.0,
+      color: Colors.white70,
     ),
     bodyMedium: TextStyle(
       fontSize: 12.0,
       height: 1.5,
+      color: Colors.white70,
     ),
     bodySmall: TextStyle(
       fontSize: 10.0,
       height: 1.5,
+      color: Colors.white70,
     ),
   );
 }
