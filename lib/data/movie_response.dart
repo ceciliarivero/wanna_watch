6c135392099b56ed08a5cd83dae6781fd0fc2974
Cecilia Rivero <contact@ceciliarivero.com>
2023-03-22 20:11:55 +0000

    Update MovieResponse class.

--- lib/data/movie_response.dart
+++ lib/data/movie_response.dart
@@ -4,13 +4,22 @@ part 'movie_response.g.dart';
 
 @JsonSerializable(createToJson: false)
 class MovieResponse {
-  final int id;
+  final int? id;
   final String? title;
   final String? overview;
   @JsonKey(name: 'poster_path')
   final String? posterPath;
   @JsonKey(name: 'release_date')
   final String? releaseDate;
+  @JsonKey(name: 'backdrop_path')
+  final String? backdropPath;
+  final List<Map<String, dynamic>>? genres;
+  @JsonKey(name: 'original_language')
+  final String? originalLanguage;
+  @JsonKey(name: 'original_title')
+  final String? originalTitle;
+  @JsonKey(name: 'production_countries')
+  final List<Map<String, dynamic>>? productionCountries;
 
   MovieResponse({
     required this.id,
@@ -18,6 +27,11 @@ class MovieResponse {
     required this.overview,
     required this.posterPath,
     required this.releaseDate,
+    required this.backdropPath,
+    required this.genres,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.productionCountries,
   });
 
   factory MovieResponse.fromJson(Map<String, dynamic> json) =>
--- lib/data/movie_response.g.dart
+++ lib/data/movie_response.g.dart
@@ -8,9 +8,18 @@ part of 'movie_response.dart';
 
 MovieResponse _$MovieResponseFromJson(Map<String, dynamic> json) =>
     MovieResponse(
-      id: json['id'] as int,
+      id: json['id'] as int?,
       title: json['title'] as String?,
       overview: json['overview'] as String?,
       posterPath: json['poster_path'] as String?,
       releaseDate: json['release_date'] as String?,
+      backdropPath: json['backdrop_path'] as String?,
+      genres: (json['genres'] as List<dynamic>?)
+          ?.map((e) => e as Map<String, dynamic>)
+          .toList(),
+      originalLanguage: json['original_language'] as String?,
+      originalTitle: json['original_title'] as String?,
+      productionCountries: (json['production_countries'] as List<dynamic>?)
+          ?.map((e) => e as Map<String, dynamic>)
+          .toList(),
     );
