058c2e45156e26b745fccdfd146bea7a32aa122f
Cecilia Rivero <contact@ceciliarivero.com>
2023-03-24 16:36:29 +0000

    Add DBClient methods to movies repository.

--- lib/data/movies_repository.dart
+++ lib/data/movies_repository.dart
@@ -3,4 +3,7 @@ import './movie.dart';
 abstract class MoviesRepository {
   Future<List<Movie>> getMovies();
   Future<Movie> getMovie(int id);
+  Future<void> addMovieToWatchList(Movie movie);
+  Future<void> removeMovieFromWatchList(int id);
+  Future<List<Movie>> getMoviesFromWatchList();
 }
--- lib/data/network_movies_repository.dart
+++ lib/data/network_movies_repository.dart
@@ -1,11 +1,17 @@
-import './movies_repository.dart';
+import './db_client.dart';
 import './movie.dart';
+import './movie_to_watch_request_body.dart';
 import './movies_api_client.dart';
+import './movies_repository.dart';
 
 class NetworkMoviesRepository implements MoviesRepository {
   final MoviesApiClient _moviesApiClient;
+  final DBClient _dbClient;
 
-  NetworkMoviesRepository(this._moviesApiClient);
+  NetworkMoviesRepository(
+    this._moviesApiClient,
+    this._dbClient,
+  );
 
   @override
   Future<List<Movie>> getMovies() async {
@@ -18,5 +24,36 @@ class NetworkMoviesRepository implements MoviesRepository {
   Future<Movie> getMovie(int id) async {
     final response = await _moviesApiClient.getMovie(id);
     return Movie.fromResponse(response);
+  }
+
+  @override
+  Future<void> addMovieToWatchList(Movie movie) async {
+    final body = MovieToWatchWrapperRequestBody(
+      data: MovieToWatchRequestBody(
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        posterPath: movie.posterPath,
+        releaseYear: movie.releaseYear,
+        backdropPath: movie.backdropPath,
+        genres: movie.genres,
+        originalLanguage: movie.originalLanguage,
+        originalTitle: movie.originalTitle,
+        productionCountries: movie.productionCountries,
+      ),
+    );
+    await _dbClient.addMovieToWatchList(body);
+  }
+
+  @override
+  Future<void> removeMovieFromWatchList(int id) async {
+    await _dbClient.removeMovieFromWatchList(id);
+  }
+
+  @override
+  Future<List<Movie>> getMoviesFromWatchList() async {
+    final response = await _dbClient.getMoviesFromWatchList();
+
+    return response.map((r) => Movie.fromMovieToWatchResponse(r)).toList();
   }
 }
