4c27e3153ba26a0aa5d0eb1dd67d2e02a859a645
Cecilia Rivero <contact@ceciliarivero.com>
2023-03-24 16:35:19 +0000

    Add fromMovieToWatchResponse to Movie model.

--- lib/data/movie.dart
+++ lib/data/movie.dart
@@ -1,5 +1,7 @@
-import './movie_response.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import './movie_response.dart';
+import './movie_to_watch_response.dart';
 
 class Movie {
   final int id;
@@ -36,6 +38,30 @@ class Movie {
             ?.map((productionCountry) => productionCountry['name'].toString())
             .toList() ??
         [];
+
+    return Movie(
+      id: response.id ?? 0,
+      title: response.title ?? '',
+      overview: response.overview ?? '',
+      posterPath: response.posterPath != null
+          ? '$moviesApiImagesURL${response.posterPath}'
+          : '',
+      releaseYear: releaseYear,
+      backdropPath: response.backdropPath != null
+          ? '$moviesApiImagesURL${response.backdropPath}'
+          : '',
+      genres: genres,
+      originalLanguage: response.originalLanguage ?? '',
+      originalTitle: response.originalTitle ?? '',
+      productionCountries: productionCountries,
+    );
+  }
+
+  factory Movie.fromMovieToWatchResponse(MovieToWatchResponse response) {
+    final moviesApiImagesURL = dotenv.env['MOVIES_API_IMAGES_URL'];
+    final releaseYear = response.releaseDate?.substring(0, 4) ?? '';
+    final genres = response.genres ?? [];
+    final productionCountries = response.productionCountries ?? [];
 
     return Movie(
       id: response.id ?? 0,
